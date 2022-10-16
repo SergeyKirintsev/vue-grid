5f829ea2ae9fdedc3342e8fa1894ed920b1c96f6
SergeyKirintsev <kserg80@gmail.com>
2022-10-16 03:16:40 +0300

    refactoring

---
+++ src/json/data.js
@@ -0,0 +1,110 @@
+export const arr = [
+  {
+    id: 1,
+    name: 'Бюллетени33',
+    children: [
+      {
+        id: 1,
+        name: 'Type 11',
+        children: [
+          {
+            id: 1,
+            name: 'Русский',
+            children: [
+              {
+                id: 101,
+                name: 'type 1 rus',
+                children: [
+                  {
+                    id: 1,
+                    name: 'Documents 10 rus'
+                  }
+                ]
+              } 
+            ],
+          },
+          {
+            id: 1,
+            name: 'English',
+            children: [
+              {
+                id: 102,
+                name: 'type 1 eng',
+                children: [
+                  {
+                    id: 1,
+                    name: 'Documents 10 eng'
+                  }
+                ]
+              } 
+            ],
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Type 2',
+        children: [
+          {
+            id: 1,
+            name: 'Японский',
+            children: [
+              {
+                id: 101,
+                name: 'type 2 japan',
+                children: [
+                  {
+                    id: 1,
+                    name: 'Documents 10 japan'
+                  }
+                ]
+              } 
+            ],
+          },
+          {
+            id: 1,
+            name: 'English',
+            children: [
+              {
+                id: 102,
+                name: 'type 100500 eng',
+                children: [
+                  {
+                    id: 1,
+                    name: 'Documents 100500 eng'
+                  }
+                ]
+              },
+              {
+                id: 102,
+                name: 'type 22 eng',
+                children: [
+                  {
+                    id: 1,
+                    name: 'Documents 22 eng'
+                  }
+                ]
+              } 
+            ],
+          },
+          {
+            id: 1,
+            name: 'Корейский',
+            children: [
+              {
+                id: 101,
+                name: 'type 2 Корейский',
+                children: [
+                  {
+                    id: 1,
+                    name: 'Documents 10111 Корейский'
+                  }
+                ]
+              } 
+            ],
+          },
+        ]
+      },
+    ]
+  }
+]
